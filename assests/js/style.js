94c489f1e6989ae066ff149c0004df3ff151d66f
DillerDiller <dylan.halligan@hotmail.com>
2022-06-29 12:13:47 +0000

    Set up structure of game functions

---
+++ assests/js/style.js
@@ -0,0 +1,37 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+ function calculateCorrectAnswer(){
+
+ }
+
+ function incrementScore(){
+
+ }
+
+ function incrementWrongAnswer(){
+
+ }
+
+ function displayQuestionOne(){
+
+ }
+
+ function displayQuestionTwo(){
+
+ }
+ function displayQuestionThree(){
+
+ }
+ function displayQuestionFour(){
+
+ }
+
+ function displayQuestionFive(){
+    
+ }
