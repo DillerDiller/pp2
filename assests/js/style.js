c61872c4150792ea7f95ef47ee7ba8c28bb9052c
DillerDiller <dylan.halligan@hotmail.com>
2022-07-07 17:02:08 +0000

    Add Event listener and function

--- assests/js/style.js
+++ assests/js/style.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let options = document.getElementsByTagName("button");
+
+    for (let i = 0; i <options.length; i++){
+        option1.addEventListener("click" , function(){
+            if (this.getAttribute("data-type") === "option1"){
+                alert("You clicked Option1");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked Option1`);
+            }
+        })
+    }
+})
+
 function runGame(){
 
 }
