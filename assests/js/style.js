d286843efa55b5b144d7ad2e1d8b1cd148d4775f
DillerDiller <dylan.halligan@hotmail.com>
2022-07-09 06:36:30 +0000

    Add questions in JavaScript

--- assests/js/style.js
+++ assests/js/style.js
@@ -44,6 +44,43 @@ function checkAnswer(){
             {text: "1985", correct: true},
             {text:"1984", correct: false},
             {text:"1986", correct: false},
+            {text: "1983", correct: false},
         ]
-    }
+    },
+    {
+        question: "Who Designed the Jordan 1",
+        answer: [
+            {text: "Michael Jordan", correct: false},
+            {text:"Tinker Hatfield", correct: false},
+            {text:"Phil Knight", correct: false},
+            {text: "Peter Moore", correct: true},
+        ]
+    },
+    {
+        question: "Who designed the Air Max 1?",
+        answer: [
+            {text: "Peter Moore", correct: false},
+            {text:"Tinker Hatfield", correct: true},
+            {text:"Tobie Hatfield", correct: false},
+            {text: "Phil Knight", correct: false},
+        ]
+    },
+    {
+        question: "Who currently makes Yeezy?",
+        answer: [
+            {text: "Adidas", correct: true},
+            {text:"Nike", correct: false},
+            {text:"New Balance", correct: false},
+            {text: "Puma", correct: false},
+        ]
+    },
+    {
+        question: "William J Riley(founder of New Balance), is from?",
+        answer: [
+            {text: "United States", correct: true},
+            {text:"Ireland", correct: false},
+            {text:"England", correct: false},
+            {text: "Canada", correct: false},
+        ]
+    },
  ]
